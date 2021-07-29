834ceb2ae2651b07279475c560b092e38bbbfab6
Seham A Alharbi <seham.alharbi@yahoo.com>
2021-07-29 16:59:53 +0100

    version presented at meeting

--- GenericGraph/src/com/graph/Graph.java
+++ GenericGraph/src/com/graph/Graph.java
@@ -2,60 +2,60 @@ package com.graph;
 
 import java.util.*;
 
-
 /**
- * @author SehamAlharbi
- * The whole graph is represented by K keys - start - vertices and a list of their neighbours - Node objects
- * @param <K> vertices 
+ * @author SehamAlharbi The whole graph is represented by K keys - start -
+ *         vertices and a list of their neighbours - Node objects
+ * @param <K> vertices
  * @param <E> relationships
  */
 
-public class Graph <K,E> {
-	
+public class Graph<K, E> {
+
 	// Representation of each node, and its edges with the other nodes
-	Map <K, List<Node<K,E>>> graph = null;
-	
-	public Graph (){
-		graph = new HashMap <> ();
+	Map<K, List<Node<K, E>>> graph = null;
+
+	public Graph() {
+		graph = new HashMap<>();
 	}
-	
+
 	/**
 	 * This adds a direct edge from v1 to v2 in directed graph
-	 * @param v1 start vertex 
-	 * @param v2 end vertex
-	 * @param edgeLable the weight 
+	 * 
+	 * @param v1        start vertex
+	 * @param v2        end vertex
+	 * @param edgeLable the weight
 	 * @return
 	 */
-	public boolean addDirectEdge( K v1 , K v2, E edgeLabel) {
-		
+	public boolean addDirectEdge(K v1, K v2, E edgeLabel) {
+
 		if (!graph.containsKey(v1)) {
 			graph.put(v1, new LinkedList<>());
 		}
-		
-		graph.get(v1).add(new Node<>(v1,v2,edgeLabel));
-	
-	return true;
-}
-	
+
+		graph.get(v1).add(new Node<>(v1, v2, edgeLabel));
+
+		return true;
+	}
+
 	/**
 	 * This adds an undirected edge between both v1 and v2 in undirected graph
-	 * @param v1 start vertex
-	 * @param v2 end vertex
+	 * 
+	 * @param v1        start vertex
+	 * @param v2        end vertex
 	 * @param edgeLabel the weight
 	 * @return
 	 */
-	public boolean addIndirectEdge (K v1 , K v2, E edgeLabel) {
-		addDirectEdge (v1, v2, edgeLabel);
-		addDirectEdge (v2, v1, edgeLabel);
-		
+	public boolean addIndirectEdge(K v1, K v2, E edgeLabel) {
+		addDirectEdge(v1, v2, edgeLabel);
+		addDirectEdge(v2, v1, edgeLabel);
+
 		return true;
 	}
-	
+
 	/**
 	 * get the nodes and send them to the method that implements the DFS algorithm
 	 */
 	public boolean hasPathDFS(K source, K destination) {
-
 
 		// a flag to keep track of the visited node to avoid infinite calls
 		HashSet<K> visited = new HashSet<K>();
@@ -65,10 +65,11 @@ public class Graph <K,E> {
 		return hasPathDFS(source, destination, visited);
 
 	}
-	
-	
+
 	/**
-	 * overloaded recursive method that does the DFS algorithm implementation, stack-like implementation
+	 * overloaded recursive method that does the DFS algorithm implementation,
+	 * stack-like implementation
+	 * 
 	 * @param source
 	 * @param destination
 	 * @param visited
@@ -93,38 +94,29 @@ public class Graph <K,E> {
 
 		// final option is to start looping through all adjacent nodes and their
 		// adjacent nodes till finding -d-
-		
-		for (K child : graph.keySet()) {
-			if (hasPathDFS(child, destination, visited)) {
+		for (Node<K, E> child : graph.get(source)) {
+			if (hasPathDFS(child.endNode, destination, visited)) {
 				return true;
 			}
 		}
-		
-		
-//		for (Node child : source.adjacent) {
-//			// recursive call
-//			if (hasPathDFS(child, destination, visited)) {
-//				return true;
-//			}
-//		}
 
 		// if after all, no path is found
 		return false;
 	}
-	
-	
+
 	/**
-	 *  To print the graph data whenever needed
+	 * To print the graph data whenever needed
 	 */
-	public String toString () {
+	public String toString() {
 		String data = "";
 		for (K key : graph.keySet()) {
-			
-			// print the value of the key - the value is nothing but a list of Node objects - ..
+
+			// print the value of the key - the value is nothing but a list of Node objects
+			// - ..
 			// toString method of the class Node is called in each loop
 			data += key + "==> [ " + graph.get(key) + "] \n";
 		}
-		
+
 		return data;
 	}
 
--- GenericGraph/src/com/graph/Node.java
+++ GenericGraph/src/com/graph/Node.java
@@ -3,54 +3,54 @@ package com.graph;
 import java.util.UUID;
 
 /**
- * class to represent each vertex - each got key value -> edgeLabel to -> end node
+ * class to represent each vertex - each got key value -> edgeLabel to -> end
+ * node
+ * 
  * @author SehamAlharbi
  * @param <K> vertices
  * @param <E> relationships
  */
 
-public class Node <K, E>{
-	
+public class Node<K, E> {
+
 	K key = null;
 	K endNode = null;
 	E edgeLabel = null;
 
-	
-	
-	public Node (K key, K endNode, E edgeLable) {
+	public Node(K key, K endNode, E edgeLable) {
 		this.key = key;
 		this.endNode = endNode;
 		this.edgeLabel = edgeLable;
-		
-	} 
-	
+
+	}
+
 	/**
-	 * Because the key -K- is not represented by any type e.g., String, Integer .. that would be easy to be compared to each other ..
-	 * We need to override the equals method to do the equality check
+	 * Because the key -K- is not represented by any type e.g., String, Integer ..
+	 * that would be easy to be compared to each other .. We need to override the
+	 * equals method to do the equality check
 	 */
 	@Override
 	public boolean equals(Object node) {
 		return key.equals(node);
 	}
-	
+
 	/**
-	 * If two objects are equal according to the equals(Object) method, 
-	 * calling the hashCode() method on each of the two objects must produce the same value.
+	 * If two objects are equal according to the equals(Object) method, calling the
+	 * hashCode() method on each of the two objects must produce the same value.
 	 */
 	@Override
-	public int hashCode () {
+	public int hashCode() {
 		return key.hashCode();
 	}
-	
-	
-	public UUID generateID () {
+
+	public UUID generateID() {
 		return UUID.randomUUID();
 	}
-	
+
 	/**
 	 * To print the node data whenever needed
 	 */
-	public String toString () {
-		return " { Key: " + key + " , End Node: " + endNode + " , Edge Label: " + edgeLabel + " } " ;
+	public String toString() {
+		return " { Key: " + key + " , End Node: " + endNode + " , Edge Label: " + edgeLabel + " } ";
 	}
 }
